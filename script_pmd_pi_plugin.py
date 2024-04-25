49b5446eb52d63bef7bd06c8e69c5f1a46413ea5
NicolasGuenaux <nicolas.guenaux@cea.fr>
2024-04-25 16:37:58 +0200

    premier essai.. but: enregistrer le plugin pour pouvoir l'ouvrir avec daq_move

---
+++ script_pmd_pi_plugin.py
@@ -0,0 +1,5 @@
+from pipython import GCSDevice
+pidevice = GCSDevice('C-884')
+pidevice.InterfaceSetupDlg()
+print(pidevice.qIDN())
+pidevice.CloseConnection()
--- src/pymodaq_plugins_siglent/daq_move_plugins/daq_move_Siglent.py
+++ src/pymodaq_plugins_siglent/daq_move_plugins/daq_move_Siglent.py
@@ -2,11 +2,13 @@ from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_pa
     DataActuator  # common set of parameters for all actuators
 from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
 from pymodaq.utils.parameter import Parameter
+from ..hardware.siglent_wrapper import ActuatorWrapper
 
 
-class PythonWrapperOfYourInstrument:
-    #  TODO Replace this fake class with the import of the real python wrapper of your instrument
-    pass
+# class PythonWrapperOfYourInstrument:
+#     #  TODO Replace this fake class with the import of the real python wrapper of your instrument
+#     pass
+## No python wrapper for Siglent SDG products (no dll files, only SCPI commands)
 
 # TODO:
 # (1) change the name of the following class to DAQ_Move_TheNameOfYourChoice
@@ -14,31 +16,32 @@ class PythonWrapperOfYourInstrument:
 #     for the class name and the file name.)
 # (3) this file should then be put into the right folder, namely IN THE FOLDER OF THE PLUGIN YOU ARE DEVELOPING:
 #     pymodaq_plugins_my_plugin/daq_move_plugins
-class DAQ_Move_Template(DAQ_Move_base):
+class DAQ_Move_Siglent(DAQ_Move_base):
     """ Instrument plugin class for an actuator.
     
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
     DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
 
-    TODO Complete the docstring of your plugin with:
-        * The set of controllers and actuators that should be compatible with this instrument plugin.
-        * With which instrument and controller it has been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
+    # TODO Complete the docstring of your plugin with:
+    #     * The set of controllers and actuators that should be compatible with this instrument plugin.
+    #     * With which instrument and controller it has been tested.
+    #     * The version of PyMoDAQ during the test.
+    #     * The version of the operating system.
+    #     * Installation instructions: what manufacturer’s drivers should be installed to make it run?
 
     Attributes:
     -----------
     controller: object
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
+
+    
     # TODO add your particular attributes here if any
 
     """
-    _controller_units = 'whatever'  # TODO for your plugin: put the correct unit here
-    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
-    _axis_names = ['Axis1', 'Axis2']  # TODO for your plugin: complete the list
+    _controller_units = ActuatorWrapper.units  # TODO for your plugin: put the correct unit here
+    is_multiaxes = True  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
+    _axis_names = ['Amplitude', 'Phase']  # TODO for your plugin: complete the list
     _epsilon = 0.1  # TODO replace this by a value that is correct depending on your controller
     data_actuator_type = DataActuatorType['DataActuator']  # wether you use the new data style for actuator otherwise set this
     # as  DataActuatorType['float']  (or entirely remove the line)
@@ -48,15 +51,24 @@ class DAQ_Move_Template(DAQ_Move_base):
     # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
     # the target value. It is the developer responsibility to put here a meaningful value
 
+
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
+        # #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
+        # #  autocompletion
+        # self.controller: PythonWrapperOfYourInstrument = None
+
+        # Not sure if it is necessary to init them at this point
+        # These are the initial values when the instrument is turned on
+
+
 
         #TODO declare here attributes you want/need to init with a default value
         pass
 
-    def get_actuator_value(self):
+
+
+
+    def get_actuator_value(self): 
         """Get the current value from the hardware with scaling conversion.
 
         Returns
@@ -64,15 +76,17 @@ class DAQ_Move_Template(DAQ_Move_base):
         float: The position obtained after scaling conversion.
         """
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        pos = DataActuator(data=self.controller.your_method_to_get_the_actuator_value())  # when writing your own plugin replace this line
+        # raise NotImplemented  # when writing your own plugin remove this line
+        # pos = DataActuator(data=self.controller.your_method_to_get_the_actuator_value())  # when writing your own plugin replace this line
+        pos = self.controller.get_amplitude()
+        
         pos = self.get_position_with_scaling(pos)
         return pos
 
     def close(self):
         """Terminate the communication protocol"""
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
+        # raise NotImplemented  # when writing your own plugin remove this line
         #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
 
     def commit_settings(self, param: Parameter):
@@ -104,13 +118,14 @@ class DAQ_Move_Template(DAQ_Move_base):
             False if initialization failed otherwise True
         """
 
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
+        # raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
         self.controller = self.ini_stage_init(old_controller=controller,
-                                              new_controller=PythonWrapperOfYourInstrument())
+                                              new_controller=ActuatorWrapper())
+        self.controller.open_communication()
 
         info = "Whatever info you want to log"
-        initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # todo
-        return info, initialized
+        initialized = self.controller.__init__()  # todo
+        return info
 
     def move_abs(self, value: DataActuator):
         """ Move the actuator to the absolute target defined by value
@@ -124,9 +139,9 @@ class DAQ_Move_Template(DAQ_Move_base):
         self.target_value = value
         value = self.set_position_with_scaling(value)  # apply scaling if the user specified one
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_an_absolute_value(value.value())  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_amplitude(value)  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand('Update_Status', ['amplitude updated']))
 
     def move_rel(self, value: DataActuator):
         """ Move the actuator to the relative target actuator value defined by value
@@ -140,25 +155,25 @@ class DAQ_Move_Template(DAQ_Move_base):
         value = self.set_position_relative_with_scaling(value)
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_a_relative_value(value.value())  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_amplitude(self.target_value)  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand('Update_Status', ['amplitude updated']))
 
     def move_home(self):
         """Call the reference method of the controller"""
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_get_to_a_known_reference()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_amplitude(2)  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand('Update_Status', ['moved home (amp = 2)']))
 
     def stop_motion(self):
       """Stop the actuator and emits move_done signal"""
 
       ## TODO for your custom plugin
-      raise NotImplemented  # when writing your own plugin remove this line
-      self.controller.your_method_to_stop_positioning()  # when writing your own plugin replace this line
-      self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+    #   raise NotImplemented  # when writing your own plugin remove this line
+      self.controller.set_state("OFF")  # when writing your own plugin replace this line
+      self.emit_status(ThreadCommand('Update_Status', ['channel output OFF']))
 
 
 if __name__ == '__main__':
